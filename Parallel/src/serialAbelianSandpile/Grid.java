ad17c87c2ab45739ea80e769ea4c31e288a9dd40
Nathan <mbkmun001@myuct.ac.za>
2024-08-14 11:42:18 +0200

    bool method setup

--- Parallel/src/serialAbelianSandpile/Grid.java
+++ Parallel/src/serialAbelianSandpile/Grid.java
@@ -5,6 +5,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 
 //This class is for the grid for the Abelian Sandpile cellular automaton
 public class Grid {
@@ -77,26 +80,65 @@ public class Grid {
 		}
 	}
 	
-	//key method to calculate the next update grod
-	boolean update() {
-		boolean change=false;
-		//do not update border
-		for( int i = 1; i<rows-1; i++ ) {
-			for( int j = 1; j<columns-1; j++ ) {
-				updateGrid[i][j] = (grid[i][j] % 4) + 
-						(grid[i-1][j] / 4) +
-						grid[i+1][j] / 4 +
-						grid[i][j-1] / 4 + 
-						grid[i][j+1] / 4;
-				if (grid[i][j]!=updateGrid[i][j]) {  
-					change=true;
+	//key method to calculate the next update grid
+	public boolean update() {
+        ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+        UpdateTask mainTask = new UpdateTask(1, rows - 2, 1, columns - 2);
+        Boolean change = pool.invoke(mainTask);
+		
+        if (change) {
+            nextTimeStep();
+        }
+        return change;
+    }
+	//Added class to proccess the parallelism task
+	private class UpdateTask extends RecursiveTask<Boolean> {
+		private static final int THRESHOLD = 500; 
+		private int startRow, endRow, startCol, endCol;
+	
+		public UpdateTask(int startRow, int endRow, int startCol, int endCol) {
+			this.startRow = startRow;
+			this.endRow = endRow;
+			this.startCol = startCol;
+			this.endCol = endCol;
+		}
+	
+		@Override
+		protected Boolean compute() {
+			if ((endRow - startRow) * (endCol - startCol) <= THRESHOLD) {
+				return updatePart();
+			} else {
+				int midRow = (startRow + endRow) / 2;
+				int midCol = (startCol + endCol) / 2;
+	
+				UpdateTask topLeft = new UpdateTask(startRow, midRow, startCol, midCol);
+				UpdateTask topRight = new UpdateTask(startRow, midRow, midCol + 1, endCol);
+				UpdateTask bottomLeft = new UpdateTask(midRow + 1, endRow, startCol, midCol);
+				UpdateTask bottomRight = new UpdateTask(midRow + 1, endRow, midCol + 1, endCol);
+	
+				invokeAll(topLeft, topRight, bottomLeft, bottomRight);
+	
+				return topLeft.join() || topRight.join() || bottomLeft.join() || bottomRight.join();
+			}
+		}
+	
+		private Boolean updatePart() {
+			boolean change = false;
+			for (int i = startRow; i <= endRow; i++) {
+				for (int j = startCol; j <= endCol; j++) {
+					updateGrid[i][j] = (grid[i][j] % 4) + 
+									   (grid[i - 1][j] / 4) +
+									   grid[i + 1][j] / 4 +
+									   grid[i][j - 1] / 4 + 
+									   grid[i][j + 1] / 4;
+					if (grid[i][j] != updateGrid[i][j]) {
+						change = true;
+					}
 				}
-		}} //end nested for
-	if (change) { nextTimeStep();}
-	return change;
+			}
+			return change;
+		}
 	}
-	
-	
 	
 	//display the grid in text format
 	void printGrid( ) {
